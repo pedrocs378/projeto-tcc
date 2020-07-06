30e6583ff750de3fefbcdec16ea26331ccc7f1e0
Pedro Cesar <pedrocs378@gmail.com>
2020-07-06 18:44:38 -0300

    adicionando paginação

--- frontend/src/pages/SearchResults/index.js
+++ frontend/src/pages/SearchResults/index.js
@@ -1,14 +1,119 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { MemoryRouter, Route } from 'react-router'
+import { useHistory, Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import Pagination from '@material-ui/lab/Pagination'
+import PaginationItem from '@material-ui/lab/PaginationItem'
 
 import './styles.css'
 
 import SearchBar from '../../components/SearchBar'
 
+const results = [
+    {
+        id: 0,
+        url: 'http://www.google.com',
+        title: 'Google',
+        desc: 'Search the worlds information, including webpages, images, videos and more.'
+    },
+    {
+        id: 1,
+        url: 'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi_6cufsbnqAhUhJLkGHQADCQYQFjABegQIARAB&url=https%3A%2F%2Fwww.google.com%2F%3Fhl%3Dpt_br&usg=AOvVaw2LUYq2HB3rrKwdSYM0-Zvl',
+        title: 'Google',
+        desc: 'Pesquisa · Imagens · Maps · Play · YouTube · Notícias · Gmail · Drive · Mais · Agenda · Tradutor · Google Mobile · Livros · Shopping · Blogger · Fotos · Vídeos ...'
+    },
+    {
+        id: 2,
+        url: 'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi_6cufsbnqAhUhJLkGHQADCQYQFjACegQIBBAB&url=https%3A%2F%2Fadssettings.google.com%2Fauthenticated%3Fhl%3Dpt-BR&usg=AOvVaw2sfhum3LHP2y7HNo5K3aXV',
+        title: 'Fazer login nas Contas do Google',
+        desc: 'Use sua Conta do Google. E-mail ou telefone. Esqueceu seu e-mail? Digite o texto que você ouve ou vê. Não está no seu computador? Use o modo visitante ...'
+    },
+    {
+        id: 3,
+        url: 'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi_6cufsbnqAhUhJLkGHQADCQYQFjADegQIBRAB&url=https%3A%2F%2Fmyaccount.google.com%2Fintro%3Fhl%3Dpt-BR&usg=AOvVaw2hauLPVV_ljaApq6-LF-mM',
+        title: 'Conta do Google',
+        desc: 'Quando você faz login na sua Conta do Google, pode ver e gerenciar suas ... de privacidade para ajudar o Google a atender melhor suas necessidades.'
+    },
+    {
+        id: 4,
+        url: 'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi_6cufsbnqAhUhJLkGHQADCQYQFjAEegQIEBAB&url=https%3A%2F%2Fcanaltech.com.br%2Fapps%2Fgoogle-play-store-recebe-novos-detalhes-visuais-nos-menus-de-categorias-167575%2F&usg=AOvVaw2OxvbZf1pJfGdlKnnZrf7b',
+        title: 'Google Play Store recebe novos detalhes visuais nos menus ...',
+        desc: 'Os aplicativos do Google constantemente mudam de visual em testes que, não raro, limitam-se a apenas uma pequena parcela de celulares ...'
+    },
+    {
+        id: 5,
+        url: 'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi_6cufsbnqAhUhJLkGHQADCQYQFjAFegQIBhAB&url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.google.android.googlequicksearchbox%26hl%3Dpt_BR&usg=AOvVaw2WpR3TC46VcEl0-jbbq1oo',
+        title: 'Google',
+        desc: 'Site da Google'
+    },
+    {
+        id: 6,
+        url: 'http://play.google.com/store',
+        title: 'Google',
+        desc: 'Site da Google'
+    },
+    {
+        id: 7,
+        url: 'http://www.google.com',
+        title: 'Google',
+        desc: 'Site da Google'
+    },
+    {
+        id: 8,
+        url: 'http://www.google.com',
+        title: 'Google',
+        desc: 'Site da Google'
+    },
+    {
+        id: 9,
+        url: 'http://www.google.com',
+        title: 'Google',
+        desc: 'Site da Google'
+    },
+    {
+        id: 10,
+        url: 'http://www.google.com',
+        title: 'Google',
+        desc: 'Site da Google'
+    },
+    {
+        id: 11,
+        url: 'http://www.google.com',
+        title: 'Google',
+        desc: 'Site da Google'
+    },
+]
+
 export default function SearchResults() {
+    const [page, setPage] = useState(1)
+    const [pageResults, setPageResults] = useState([])
+
     const history = useHistory()
 
+    let cont = 0
+    useEffect(() => {
+        if (results.length > 0) {
+            let page = 1
+            const pageResultsAux = []
+            for (let i = 0; i < results.length; i++) {
+                pageResultsAux[i] = results[i]
+                pageResultsAux[i].page = page
+                cont++
+                if (cont === 10) {
+                    cont = 0
+                    page++
+                }
+            }
+            pageResultsAux.totalPages = page
+            setPageResults(pageResultsAux)
+            console.log(pageResultsAux)
+        }
+    }, [cont])
+
+
+    function handleChange(event, page) {
+        setPage(page)
+    }
 
     return (
         <div id="results-container">
@@ -28,72 +133,49 @@ export default function SearchResults() {
             <div id="resultsbar">
                 <div className="results">
                     <ul>
-                        <li>
-                            <a href="http://www.google.com.br">
-                                <cite>
-                                    http://www.google.com.br
-                                </cite>
-                                <h3>Titulo</h3>
-                            </a>               
-                            <span>Descrição</span>
-                        </li>
-                        <li>
-                            <a href="http://www.google.com.br">
-                                <cite>
-                                    http://www.google.com.br
-                                </cite>
-                                <h3>Titulo</h3>
-                            </a>               
-                            <span>Descrição</span>
-                        </li>
-                        <li>
-                            <a href="http://www.google.com.br">
-                                <cite>
-                                    http://www.google.com.br
-                                </cite>
-                                <h3>Titulo</h3>
-                            </a>               
-                            <span>Descrição</span>
-                        </li>
-                        <li>
-                            <a href="http://www.google.com.br">
-                                <cite>
-                                    http://www.google.com.br
-                                </cite>
-                                <h3>Titulo</h3>
-                            </a>               
-                            <span>Descrição</span>
-                        </li>
-                        <li>
-                            <a href="http://www.google.com.br">
-                                <cite>
-                                    http://www.google.com.br
-                                </cite>
-                                <h3>Titulo</h3>
-                            </a>               
-                            <span>Descrição</span>
-                        </li>
-                        <li>
-                            <a href="http://www.google.com.br">
-                                <cite>
-                                    http://www.google.com.br
-                                </cite>
-                                <h3>Titulo</h3>
-                            </a>               
-                            <span>Descrição</span>
-                        </li>
-                        <li>
-                            <a href="http://www.google.com.br">
-                                <cite>
-                                    http://www.google.com.br
-                                </cite>
-                                <h3>Titulo</h3>
-                            </a>               
-                            <span>Descrição</span>
-                        </li>
-                        
+                        {
+                            pageResults.map(result => {
+                                return (
+                                    result.page === page ?
+                                        <li key={result.id}>
+                                            <a href={result.url}>
+                                                <cite>
+                                                    {result.url.split('/')[2]}
+                                                </cite>
+                                                <h3>{result.title}</h3>
+                                            </a>               
+                                            <span>{result.desc}</span>
+                                        </li> : null 
+                                )})
+                        }                                                           
                     </ul>
                 </div>
+            </div>
+            <div className="pagination">
+                { results.length > 0 ? 
+                    <MemoryRouter initialEntries={['/search']} initialIndex={0}>
+                        <Route>
+                            {() => {
+                                return (
+                                    <Pagination 
+                                        count={pageResults.totalPages}
+                                        page={page}
+                                        onChange={handleChange}
+                                        color="primary" 
+                                        renderItem={(item) => (
+                                            <PaginationItem 
+                                                component={Link}
+                                                to={'/search/' + item.page}
+                                                {...item}
+                                            />
+                                        )}
+                                    />
+                                )
+                            }}
+                        </Route>
+                    </MemoryRouter> : 
+                    <Pagination style={{display: 'none'}} color="primary" /> 
+                }           
             </div>
         </div>
     )

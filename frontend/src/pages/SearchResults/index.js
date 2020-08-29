1b743fde6ec125dc3e576096ba50759651e5e2dc
Pedro Cesar <pedrocs378@gmail.com>
2020-08-29 08:06:07 -0300

    update frontend

--- frontend/src/pages/SearchResults/index.js
+++ frontend/src/pages/SearchResults/index.js
@@ -14,40 +14,6 @@ import SearchNotFound from '../../components/SearchNotFound'
 
 import api from '../../services/api'
 
-// const results = [
-//     {
-//         id: 0,
-//         url: 'http://www.google.com',
-//         title: 'Google',
-//         desc: 'Search the worlds information, including webpages, images, videos and more.'
-//     },
-//     {
-//         id: 1,
-//         url: 'https://www.uol.com.br',
-//         title: 'UOL - O melhor conteúdo',
-//         desc: 'Search the worlds information, including webpages, images, videos and more.'
-//     },
-//     {
-//         id: 2,
-//         url: 'http://www.correios.com.br/',
-//         title: 'Correios: encomendas, rastreamento, telegramas, cep, cartas ...',
-//         desc: 'Search the worlds information, including webpages, images, videos and more.'
-//     },
-//     {
-//         id: 3,
-//         url: 'https://www.androidpolice.com',
-//         title: 'Android Police - Android news, reviews, apps, games, phones ...',
-//         desc: 'Search the worlds information, including webpages, images, videos and more.'
-//     },
-//     {
-//         id: 4,
-//         url: 'https://www.americanas.com.br/',
-//         title: 'Americanas - Tudo. A toda hora. Em qualquer lugar.',
-//         desc: 'Search the worlds information, including webpages, images, videos and more.'
-//     },
-    
-// ]
-
 export default function SearchResults(props) {
     const [page, setPage] = useState(1)
     const [loaded, setLoaded] = useState(true)
@@ -57,14 +23,15 @@ export default function SearchResults(props) {
     useEffect(() => {
         async function loadResults() {
             const timer = setInterval(updateTimer, 1)
-
             setLoaded(false)
+
             const { data } = await api.get('/search?q=' + props.match.params.query)
             console.log(data.dataSearched)
             setResults(data)
-            setLoaded(true)
 
+            setLoaded(true)
             setPage(1)
+
             clearInterval(timer)
         }
 
@@ -73,10 +40,6 @@ export default function SearchResults(props) {
         }
         loadResults()
     }, [props.match.params.query])
-
-    function handlePageChange(event, page) {
-        setPage(page)
-    } 
 
     return (
         <div id="results-container">
@@ -110,7 +73,7 @@ export default function SearchResults(props) {
                                         className="pagination"
                                         count={results.totalPages}
                                         page={page}
-                                        onChange={handlePageChange}
+                                        onChange={(e, page) => setPage(page)}
                                         color="primary"
                                         renderItem={(item) => (
                                             <PaginationItem

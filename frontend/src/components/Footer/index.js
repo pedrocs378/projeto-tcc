5da52aae75694d9358bac22b42860c259d283587
Pedro Cesar <pedrocs378@gmail.com>
2020-10-05 09:54:30 -0300

    adicao do footer

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './styles.css'
+
+export default function Footer() {
+
+    return (
+        <footer className="footer-content">
+            <span>
+                Developed with 💙 by
+                    <strong> Pedro </strong>
+                    and
+                    <strong> Lucas </strong>
+            </span>
+        </footer>
+    )
+}

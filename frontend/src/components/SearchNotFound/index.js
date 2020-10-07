cc0950690475cc5c8f3025fe0afcb97b02fb1304
Pedro Cesar <pedrocs378@gmail.com>
2020-10-07 18:38:20 -0300

    arrumando estilizacoes do front

--- frontend/src/components/SearchNotFound/index.js
+++ frontend/src/components/SearchNotFound/index.js
@@ -5,12 +5,10 @@ import './styles.css'
 export default function SearchNotFound(props) {
 
     return (
-        <div id="not-found-container">
-            <div className="warning-card">
-                <p>
-                    Sua pesquisa: "<em>{props.value}</em>" não encontrou nenhum resultado correspondente.
-                </p>
-            </div>
+        <div className="warning-card">
+            <p>
+                Sua pesquisa: "<em>{props.value}</em>" não encontrou nenhum resultado correspondente.
+            </p>
         </div>
     )
 }
